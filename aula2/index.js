fa111c21771ce1a2dec67d1f8438679a7ac414e5
paulo henrique <phtczeferino@gmail.com>
2023-02-02 13:07:15 -0300

    numero de tentativas atualizado

--- aula2/index.js
+++ aula2/index.js
@@ -1,31 +1,31 @@
 
 function jogar() {
-    var numeroSecreto = 1 + parseInt(Math.random() * 1000);
+  var numeroSecreto = 1 + parseInt(Math.random() * 1000);
 
-    var numeroTentativas = 5;
+  var numeroTentativas = 10;
 
-    while (numeroEscolhido != numeroSecreto) {
-        var numeroEscolhido = prompt('Digite um numero de 1 a 1000')
+  while (numeroEscolhido != numeroSecreto) {
+    var numeroEscolhido = prompt('Digite um numero de 1 a 1000')
 
 
-        if (numeroEscolhido == numeroSecreto) {
-            alert(`Parabéns, você acertou!! o número é : ${numeroSecreto} 
+    if (numeroEscolhido == numeroSecreto) {
+      alert(`Parabéns, você acertou!! o número é : ${numeroSecreto} 
           tentativas restantes : ${numeroTentativas}`);
-        } else if (numeroEscolhido < numeroSecreto) {
-            alert(`Você errou... o número secreto é maior do que ${numeroEscolhido} 
+    } else if (numeroEscolhido < numeroSecreto) {
+      alert(`Você errou... o número secreto é maior do que ${numeroEscolhido} 
           tentativas: ${numeroTentativas}`);
-        } else if (numeroEscolhido > numeroSecreto) {
-            alert(`Você errou... o número secreto é menor do que ${numeroEscolhido}  
+    } else if (numeroEscolhido > numeroSecreto) {
+      alert(`Você errou... o número secreto é menor do que ${numeroEscolhido}  
           tentativas: ${numeroTentativas}`);
-        };
+    };
 
 
-        numeroTentativas -= 1
-        if (numeroTentativas === 0) {
-            alert(`Tentativas Esgotadas! O número correto era ${numeroSecreto}`)
-            break
-        }
-
+    numeroTentativas -= 1
+    if (numeroTentativas === 0) {
+      alert(`Tentativas Esgotadas! O número correto era ${numeroSecreto}`)
+      break
     }
+
+  }
 }
 
