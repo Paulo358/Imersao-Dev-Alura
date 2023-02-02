90760c8f59f28b05591362b2d46bfb8416591d0d
paulo henrique <phtczeferino@gmail.com>
2023-02-02 03:30:48 -0300

    Projeto da aula 02 adicionado

---
+++ aula2/README.md
@@ -0,0 +1,5 @@
+Neste projeto foi desenvolvido um simples jogo em que o objetivo é você adivinhar um número aleatório sorteado 
+tendo até 10 tentativas para acertar.
+
+
+Foram utilizados conceitos de laço de repetição e estrutura condicional aprendidos na Imersão Dev.
---
+++ aula2/index.js
@@ -0,0 +1,31 @@
+
+function jogar() {
+    var numeroSecreto = 1 + parseInt(Math.random() * 1000);
+
+    var numeroTentativas = 5;
+
+    while (numeroEscolhido != numeroSecreto) {
+        var numeroEscolhido = prompt('Digite um numero de 1 a 1000')
+
+
+        if (numeroEscolhido == numeroSecreto) {
+            alert(`Parabéns, você acertou!! o número é : ${numeroSecreto} 
+          tentativas restantes : ${numeroTentativas}`);
+        } else if (numeroEscolhido < numeroSecreto) {
+            alert(`Você errou... o número secreto é maior do que ${numeroEscolhido} 
+          tentativas: ${numeroTentativas}`);
+        } else if (numeroEscolhido > numeroSecreto) {
+            alert(`Você errou... o número secreto é menor do que ${numeroEscolhido}  
+          tentativas: ${numeroTentativas}`);
+        };
+
+
+        numeroTentativas -= 1
+        if (numeroTentativas === 0) {
+            alert(`Tentativas Esgotadas! O número correto era ${numeroSecreto}`)
+            break
+        }
+
+    }
+}
+
